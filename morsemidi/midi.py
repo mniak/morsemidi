6fbcecf3721c0b643d786503d0b8923dac88e363
André Guilherme <andregsilv@gmail.com>
2014-11-12 22:51:57 -0200

    Development of VLQ

--- morsemidi/midi.py
+++ morsemidi/midi.py
@@ -3,21 +3,21 @@ class MIDI():
 
 class VLQ():
     def __init__(self, input):
-        if type(input) is int:
-            self.num = input
-        elif type(input) is bytes:
-            self.num = self.bytesToNumber(input)
+        self.num = input
 
     def getBytes(self):
-        return
+        return self.numberToBytes(self.num)
 
     def getNumber(self):
         return self.num
 
     @staticmethod
-    def numberToBytes(number):
-        return b''
+    def numberToBytes(num):
+        lst = []
+        primeiro = True
+        while num > 0:
+            lst.insert(0, (num & 0x7F) | (0x80 if not primeiro else 0x00))
+            num >>= 7
+            primeiro = False
 
-    @staticmethod
-    def bytesToNumber(b):
-        return 0
+        return b''.join([chr(x) for x in lst]).rjust(4, '\x00')[-4:]
--- morsemidi/test/midi.py
+++ morsemidi/test/midi.py
@@ -8,13 +8,31 @@ class VLQTestCase(unittest.TestCase):
         pass
 
     def check(self, n, b):
-        self.assertEqual(midi.VLQ.numberToBytes(0), b, 'Wrong conversion Number->Bytes')
-        self.assertEqual(midi.VLQ.bytesToNumber(b), n, 'Wrong conversion Bytes->Number')
+        self.assertEqual(midi.VLQ.numberToBytes(n), b, 'Wrong conversion Number->Bytes')
 
+    def test_byte_length(self):
+        self.assertEqual(len(midi.VLQ.numberToBytes(0)), 4, 'Byte length must be 4')
+        self.assertEqual(len(midi.VLQ.numberToBytes(0xFFFFFFFF)), 4, 'Byte length must be 4')
+        self.assertEqual(len(midi.VLQ.numberToBytes(0xFFFFFFFFFFFFFFFFFFF)), 4, 'Byte length must be 4')
 
     def test_conversion1(self):
-        n = 0
+        n = 1
         b = b'\x00\x00\x00\x01'
+        self.check(n, b)
+
+    def test_conversion400(self):
+        n = 400
+        b = b'\x00\x00\x83\x10'
+        self.check(n, b)
+
+    def test_conversion1800(self):
+        n = 1800
+        b = b'\x00\x00\x8e\x08'
+        self.check(n, b)
+
+    def test_conversion106903(self):
+        n = 106903
+        b = b'\x00\x86\xC3\x17'
         self.check(n, b)
 
 
